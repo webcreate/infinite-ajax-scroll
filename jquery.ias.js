7a063adf070451edb3af29cc264c065224339dac
Rom <romain.hill@gmail.com>
2012-07-08 14:25:53 +0200

    Paginate trigger bug for small container fixed

--- jquery.ias.js
+++ jquery.ias.js
@@ -87,15 +87,21 @@
 		function scroll_handler()
 		{
 			
-			scrTop = scrolling_container.scrollTop();
+			// the way we calculate if have to load the next page depend on which container we have
+			if(opts.scrollInContainer){
+				scrTop = scrolling_container.offset().top;
+			}else{				
+				scrTop = scrolling_container.scrollTop();
+			}
+
 			wndHeight = scrolling_container.height();
-			
+
 			curScrOffset = scrTop + wndHeight;
 			
 			if (curScrOffset >= get_scroll_treshold()) {
-				
 				paginate(curScrOffset);
 			}
+
 		}
 		
 		/**
@@ -131,7 +137,7 @@
 			if (el.size() == 0) return 0;
 			
 			treshold = el.offset().top + el.height();
-			
+
 			if (!pure)
 				treshold += opts.tresholdMargin;
 			
@@ -147,6 +153,8 @@
 		 */
 		function paginate(curScrOffset, onCompleteHandler)
 		{
+
+			
 			urlNextPage = $(opts.next).attr("href");
 			if (!urlNextPage) return stop_scroll();
 			
